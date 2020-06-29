48bbb97d1712b9baf7731c15d621cbb71dca9b5e
John Doe <John Doe>
2020-06-29 14:31:15 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+var QRCode = require('qrcode.react'); /* https://www.npmjs.com/package/qrcode.react */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component <{}, { header: string }> {
+
+  textInput: any;
+
+  constructor(props: Readonly<{}>) {
+    super(props);
+    this.state = {header: 'www.rit.edu'};
+  }
+
+  changeHeader = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    let newHeader = this.textInput.value;
+    //console.log('submitted');
+    //update new value 
+    this.setState({header : newHeader});
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <h1 className="qrValue" > Current Value: {this.state.header}</h1>
+
+        <form onSubmit={this.changeHeader} className="form">
+            <input id="input" ref={(input) => { this.textInput = input; }} type="text" placeholder="Enter Text Here" />
+            <input type="submit" value="Submit" />
+        </form>
+
+        {/* generate a qr code*/}
+        <QRCode className="qrCode" value={this.state.header} />
+
+      </div>  
+    );
+  }
 }
 
 export default App;
