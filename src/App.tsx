ef2ac0a24cd8002279e0c7291afc9b6da992c5f6
John Doe <John Doe>
2020-06-30 09:10:36 -0500

    updated new feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,39 @@ import React from 'react';
 import './App.css';
 var QRCode = require('qrcode.react'); /* https://www.npmjs.com/package/qrcode.react */
 
-class App extends React.Component <{}, { header: string }> {
+type MyQR = { value: string };
+class App extends React.Component <{}, { qrValue: string }> {
 
   textInput: any;
+  textLevelInput: any;
 
   constructor(props: Readonly<{}>) {
     super(props);
-    this.state = {header: 'www.rit.edu'};
+    this.state = {qrValue: 'www.rit.edu'};
   }
 
-  changeHeader = (e: { preventDefault: () => void; }) => {
+  changeQr = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
-    let newHeader = this.textInput.value;
+    let newQrValue = this.textInput.value;
+    let newLevelValue = this.textLevelInput.value;
     //console.log('submitted');
     //update new value 
-    this.setState({header : newHeader});
+    this.setState({qrValue : newQrValue});
   }
 
   render() {
     return (
       <div className="App">
 
-        <h1 className="qrValue" > Current Value: {this.state.header}</h1>
+        <h1 className="qrValue" > Current Value: {this.state.qrValue}</h1>
 
-        <form onSubmit={this.changeHeader} className="form">
+        <form onSubmit={this.changeQr} className="form">
             <input id="input" ref={(input) => { this.textInput = input; }} type="text" placeholder="Enter Text Here" />
             <input type="submit" value="Submit" />
         </form>
 
         {/* generate a qr code*/}
-        <QRCode className="qrCode" value={this.state.header} />
+        <QRCode className="qrCode" level={"M"} value={this.state.qrValue} />
 
       </div>  
     );
